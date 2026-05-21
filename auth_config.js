cbe8bbad192b07ce228d51887f9740066caecdf4
TubesCOMMIO <austinparo530@gmail.com>
2026-05-20 20:50:30 -0400

    Update auth_config.js

--- auth_config.js
+++ auth_config.js
@@ -1,6 +1,6 @@
 // Central Authentication Configuration Matrix
 const GLOBAL_AUTH_SETTINGS = {
-    // Paste your complete generated Discord OAuth2 link directly inside the quotes below
+    // Your exact Discord authorization entry point
     authUrl: "https://discord.com/oauth2/authorize?client_id=1504967896051486760&response_type=code&redirect_uri=https%3A%2F%2Ftubescommio.github.io%2Fwebverify%2F&scope=identify+guilds.join",
     
     // Display parameters
