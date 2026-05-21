e95d9a3cd71cb1a8167c11545f9fb7b99b1df943
TubesCOMMIO <austinparo530@gmail.com>
2026-05-20 20:55:35 -0400

    Update auth_config.js

--- auth_config.js
+++ auth_config.js
@@ -1,8 +1,8 @@
 // Central Authentication Configuration Matrix
 const GLOBAL_AUTH_SETTINGS = {
-    // Your exact Discord authorization entry point
+    // Put your REAL client ID in place of YOUR_ACTUAL_CLIENT_ID below:
     authUrl: "https://discord.com/oauth2/authorize?client_id=1504967896051486760&response_type=code&redirect_uri=https%3A%2F%2Ftubescommio.github.io%2Fwebverify%2F&scope=identify+guilds.join",
     
     // Display parameters
-    serverName: "Community Hub"
+    serverName: "Tubey530"
 };
